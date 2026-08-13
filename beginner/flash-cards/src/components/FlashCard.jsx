62591f6543370b5294b8acb5c081b757515112a2
nanda-zone <nandaszone@gmail.com>
2026-08-13 13:38:22 +0700

    feat: build flip animation app.css

--- beginner/flash-cards/src/components/FlashCard.jsx
+++ beginner/flash-cards/src/components/FlashCard.jsx
@@ -1,14 +1,20 @@
 function FlashCard({ question, answer, showAnswer, currentCard, totalCards }) {
     return (
-        <div className="flash-card">
+        <div className={`flash-card ${showAnswer ? "flipped" : ""}`}>
             <p className="card-number">
             Card {currentCard + 1} of {totalCards}
             </p>
-            {showAnswer ? (
-            <p>{answer}</p>
-            ) : (
-            <h2>{question}</h2>
-            )}
+            <div className="flash-card-inner">
+                {showAnswer ? (
+                    <div className="flash-card-back">
+                        <p>{answer}</p>
+                    </div>
+                ) : (
+                    <div className="flash-card-front">
+                        <h2>{question}</h2>
+                    </div>
+                )}
+            </div>
         </div>
     );
 }
