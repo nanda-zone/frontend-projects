56b5cdbb12b50d2d25bb15c18187bc2213ee7726
nanda-zone <nandaszone@gmail.com>
2026-08-30 20:08:06 +0700

    feat: build timer for quiz app

--- intermediate/quiz-app/src/components/QuizCard.jsx
+++ intermediate/quiz-app/src/components/QuizCard.jsx
@@ -1,33 +1,57 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import questions from '../data/questions';
 
 function QuizCard() {
 
     const [selectedAnswer, setSelectedAnswer] = useState(null);
-    const currentQuestion = questions[0];
+    const [currentQuestion, setCurrentQuestion] = useState(0);
+    const question = questions[currentQuestion]
+    const [timeLeft, setTimeLeft] = useState(10); 
+    
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setTimeLeft((time) => time - 1)
+        }, 1000)
 
+        return () => clearInterval(timer)
+    }, [])
+
+    useEffect(() => {
+        if (timeLeft === 0) {
+            setCurrentQuestion(currentQuestion + 1)
+            setTimeLeft(10)
+            setSelectedAnswer(null)
+        }
+    }, [timeLeft])
 
     function handleAnswerClick(answer) {
-        setSelectedAnswer(answer);
+        setSelectedAnswer(answer)
+    }
+
+    function handleNext() {
+        setCurrentQuestion(currentQuestion + 1)
+        setSelectedAnswer(null)
     }
 
     return (
         <div className="quiz-card">
 
-            <p>Question 1 of {questions.length}</p>
+            <p>Question {currentQuestion + 1} of {questions.length}</p>
+
+            <p>time left: {timeLeft}s</p>
 
             <h2>
-                {currentQuestion.question}
+                {question.question}
             </h2>
 
             <div className="answers">
-                {currentQuestion.answers.map((answer) => (
+                {question.answers.map((answer) => (
                     <button
                         key={answer}
                         onClick={() => handleAnswerClick(answer)}
                         className={
                             selectedAnswer === answer
-                            ? answer === currentQuestion.correctAnswer
+                            ? answer === question.correctAnswer
                                 ? "correct"
                                 : "incorrect"
                             : ""
@@ -40,9 +64,18 @@ function QuizCard() {
 
             {selectedAnswer && (
                 <p>
-                    correct answer: {currentQuestion.correctAnswer}
+                    correct answer: {question.correctAnswer}
                 </p>
             )}
+
+            <div className="navigation">
+                <button 
+                    onClick={handleNext}
+                    disabled={!selectedAnswer}
+                >
+                    Next
+                </button>
+            </div>
 
         </div>
     );
